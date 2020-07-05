e32efdf8f2e07509ca45a1500f28530e57057e37
Kai Widdeson <kwiddeson4@gmail.com>
2020-07-05 11:29:24 +0100

    stylistic changes

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
           <input
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Enter a location..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
